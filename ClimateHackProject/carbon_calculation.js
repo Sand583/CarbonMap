ee97aa064be20b745246195b2c4eb072cda7b7a1
Safran Yusri <safranyusri@users.noreply.github.com>
2021-04-26 00:30:48 +0900

    Update carbon_calculation.js

--- ClimateHackProject/carbon_calculation.js
+++ ClimateHackProject/carbon_calculation.js
@@ -1,4 +1,6 @@
 
+Map.centerObject(aoi, 11);
+
 // LANDSAT 8
 function maskL8sr(image) {
   // Bits 3 and 5 are cloud shadow and cloud, respectively.
@@ -44,7 +46,7 @@ var maskedComposite = composite.updateMask(maskwater);
 
 var ndvi = maskedComposite.normalizedDifference(['B5', 'B4']).rename('NDVI');
 
-var ndvimask = ndvi.gt(0);
+var ndvimask = ndvi.gte(0);
 var maskedndvi = ndvi.updateMask(ndvimask);
 
 var carbon = maskedndvi.multiply(148.453).add(16.591).rename('carbon');
@@ -71,3 +73,66 @@ var currency = 'USD';
 var price = tc.multiply(market);
 
 print('Total carbon value: ', price, currency);
+
+// UI
+
+// Create an inspector panel with a horizontal layout.
+var inspector = ui.Panel({
+  layout: ui.Panel.Layout.flow('horizontal')
+});
+
+// Add a label to the panel.
+inspector.add(ui.Label('Click to get Above Ground Carbon value'));
+
+// Add the panel to the default map.
+Map.add(inspector);
+
+//Create a function to be invoked when the map is clicked
+Map.onClick(function(coords){
+  
+// Clear the panel and show a loading message.
+inspector.clear();
+inspector.style().set('shown', true);
+inspector.add(ui.Label('Loading...', {color: 'gray'}));
+  
+//Computer the carbon value
+var point = ee.Geometry.Point(coords.lon, coords.lat);
+var reduce = carbon.reduce(ee.Reducer.first());
+var sampledPoint = reduce.reduceRegion(ee.Reducer.first(), point, 30);
+var computedValue = sampledPoint.get('first');  
+
+// Request the value from the server and use the results in a function.
+computedValue.evaluate(function(result) {
+inspector.clear();
+
+// Add a label with the results from the server.
+inspector.add(ui.Label({
+      value: 'Above Ground Carbon: ' + result.toFixed(2) + ' Megagram',
+      style: {stretch: 'vertical'}
+    }));
+
+// Add a button to hide the Panel.
+    inspector.add(ui.Button({
+      label: 'Close',
+      onClick: function() {
+        inspector.style().set('shown', false);
+      }
+    }));
+  });
+});
+
+// Generate main panel and add it to the map.
+var panel = ui.Panel({style: {width:'33.333%'}});
+ui.root.insert(0,panel);
+
+// Define title and description.
+var intro = ui.Label('Carbon Map ',
+  {fontWeight: 'bold', fontSize: '24px', margin: '10px 5px'}
+);
+var subtitle = ui.Label('This area contain'+ 
+  ' Megagram.'+
+  ' Select from multiple area of interest and type of visualization; single year binary '+
+  ' or change over time.', {});
+
+// Add title and description to the panel.  
+panel.add(intro).add(subtitle);
